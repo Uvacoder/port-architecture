d1e846a2e8243d355d785aadf56da520b77ed1c4
ateliereven <orly.ev@gmail.com>
2021-11-03 11:37:22 +0200

    Add modals to images

--- pages.js
+++ pages.js
@@ -25,3 +25,30 @@ $('#contact-form').submit(function (e) {
     $(this).append(success);
 })
 
+//image modals - open and close
+var magnifyingClick = document.querySelectorAll('.magnifying');
+
+magnifyingClick.forEach(function (magnifier) {
+    magnifier.onclick = function () {
+        var img = magnifier.previousElementSibling;
+        var modalId = img.getAttribute("data-modal");
+        var modal = document.getElementById(modalId);
+        modal.style.display = "block";
+        modal.classList.add("zoom");
+        img.classList.remove("zoom-out");
+        var modalImg = modal.querySelector(".modal-content");
+        modalImg.src = img.src || modalImg.src;
+        modal.addEventListener('click', closeModal);
+        modalImg.addEventListener('click', function (e) { e.stopPropagation() })
+    }
+})
+
+function closeModal() {
+    var modal = document.querySelector('.modal.zoom');
+    modal.style.display = "none";
+    modal.classList.remove("zoom");
+    var img = document.querySelector(`[data-modal="${modal.id}"]`);
+    img.classList.add("zoom-out");
+};
+
+
